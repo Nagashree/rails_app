46188e341fc003ba271024e62283e6824e5ca2d1
Strata <nagashree.aswath@strata.co.in>
2013-02-13 11:38:03 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsApp::Application.config.secret_token = 'db8a8cc57d961ce34d0e5212dd326dba0dd537248a0ec75ead3cc3135b370d6b8bf5c513695493abd8384546d47f1ddcca7dd7347c8269dc9e17fb16167525af'
